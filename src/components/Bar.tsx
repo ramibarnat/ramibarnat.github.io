7fdf5bfd1d62d0668eff0557f04413f7bc31c893
Rami <ramisouguir@gmail.com>
2024-04-24 22:08:07 -0400

    start button finished and linkedin button adde

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -1,14 +1,20 @@
 import './Bar.css'
 import windowsIcon from '../assets/windows_icon.png'
+import linkedin from '../assets/linkedin.png'
 
 function Bar() {
     return (
         <div id='bar-container'>
             <hr id='bar-line'></hr>
             <div id='task-menu'>
-                <div id='start'>
-                    <img id="windows-icon" src={windowsIcon}></img>
-                    <p id='start-text'>Start</p>
+                <div id='left-side-task-menu'>
+                    <div id='start'>
+                        <img id="windows-icon" src={windowsIcon}></img>
+                        <p id='start-text'>Start</p>
+                    </div>
+                    <a href="https://www.linkedin.com/in/rami-souguir-9732761aa/" target="_blank">
+                        <img id="linkedin" src={linkedin}/>
+                    </a>
                 </div>
             </div>
         </div>
