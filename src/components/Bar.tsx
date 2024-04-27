51734b88684a8096c2519ed76a6b8df3683d299d
Rami <ramisouguir@gmail.com>
2024-04-27 03:35:24 -0400

    fixed bug with start menu button transition

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -9,6 +9,7 @@ function Bar() {
     const startButtonRef = useRef(null);
 
     const handleMouseUp = (event: any) => {
+        event=event;
         // if (event.target !== startButtonRef.current) {
         //     setStartButtonMouseDown((previousValue) => {
         //         if (!previousValue) {
@@ -43,7 +44,8 @@ function Bar() {
                         borderRight: startButtonMouseDown ? '1px solid rgba(255, 255, 255, 0.959)' : '2px solid rgba(14, 13, 13, 0.781)',
                         borderBottom: startButtonMouseDown ? '1px solid rgba(255, 255, 255, 0.959)' : '2px solid rgba(14, 13, 13, 0.781)',
                         marginRight: startButtonMouseDown ? '1px' : '0px',
-                        }} id="start-container" onClick={handleStartClick}>
+                        marginTop: startButtonMouseDown ? '1px' : '0px',
+                        }} id="start-container" onClick={handleStartClick} ref={startButtonRef}>
                         <div style={{
                             borderTop: startButtonMouseDown ? '1px solid rgba(0, 0, 0, 0.226)' : '1px solid rgba(222,222,222,1.0)',
                             borderLeft: startButtonMouseDown ? '1px solid rgba(0, 0, 0, 0.226)' : '1px solid rgba(222,222,222,1.0)',
