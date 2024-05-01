68b43e09344eb504e05b3bbcab6f06dc0c4d289a
Rami <ramisouguir@gmail.com>
2024-04-30 22:14:30 -0400

    fixed scrollbar issue and allowed scrolling to work in text area

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,27 +1,30 @@
 import './ScrollBar.css'
-import { useState } from 'react';
+import { useState, useRef} from 'react';
 
-function ScrollBar( onScroll: any) {
+function ScrollBar( content_ref: any ) {
     const [thumbTop, setThumbTop] = useState(0);
-    if (false) {
-        console.log(thumbTop);
-    }
+    const scrollRef = content_ref || useRef(null);
   
-    const handleScroll = (event: any) => {
-      const scrollHeight = event.target.current.scrollHeight;
-      const clientHeight = event.target.current.clientHeight;
-      const scrollTop = event.target.scrollTop;
-      const thumbRatio = scrollTop / (scrollHeight - clientHeight);
-      setThumbTop(thumbRatio * (clientHeight - event.target.current.querySelector('.thumb').offsetHeight));
-      onScroll(scrollTop); 
+    const handleScroll = () => {
+        console.log('scroll');
+        if (scrollRef.current) {
+            const scrollBar = scrollRef.current as HTMLElement
+            const scrollHeight = scrollBar.scrollHeight;
+            const clientHeight = scrollBar.clientHeight;
+            const scrollTop = scrollBar.scrollTop;
+            const thumbRatio = scrollTop / (scrollHeight - clientHeight);
+            setThumbTop(thumbRatio * (clientHeight - (scrollBar.querySelector('#scroll-bar-thumb') as HTMLElement).offsetHeight));
+        }
     };
+
     return (
         <div id='scroll-bar-margin'>
             <div id='scroll-bar-container' onScroll={handleScroll}>
-                <div id='scroll-bar-thumb' style={{top: thumbTop}}></div>
+                <div id='scroll-bar-thumb' style={{top: thumbTop}}>
+
+                </div>
             </div>
-        </div>
-        
+        </div> 
     )
 }
 
--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,28 +1,39 @@
 import './TextEditor.css'
 import ScrollBar from './ScrollBar'
+import { useRef } from 'react'
 
 interface TextEditor {
 
 }
 
 function TextEditor() {
+    const scrollableContentRef = useRef(null);
 
-
+    function handleScroll() {
+        console.log('scroll');
+    }
     return (
         <div id='text-editor-container'>
             <div id='text-body-container'>
-                <div id='text-editor-inner'>
+                <div onScroll={handleScroll} id='text-editor-inner' ref={scrollableContentRef}>
                     <div id='text-container'>
                         <h1 className='project-title'>Batle.us</h1>
                         <p className='project-body'>
-                            {`Currently developing a multiplayer game using Angular, 
+                            {`Currently developing a multiplayer game usingg. Angular, 
                             Tailwind, Node.js, Express.js, and AWS along with libraries 
                             such as D3.js, WebGL, and socket.io. Utilized Git for source control control conntorl contorl peanut peanuit peanut
                             control. (Link: `}<a href='http://www.batle.us/' target='_blank'>www.batle.us</a>)
+                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                            Here
+                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                            Here
+                            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                            EA SPORTS<br/>
+                            in game
                         </p>
                     </div>
-                {/* <ScrollBar/> */}
                 </div>
+                <ScrollBar content_ref={scrollableContentRef} />
             </div>
         </div>
     )
