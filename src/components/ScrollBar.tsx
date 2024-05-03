cdd7b848a2c13af5d11eed58f20c4a5943e014b8
Ram Sauce <ramisouguir@gmail.com>
2024-05-03 03:57:04 -0400

    scroll bar works now

--- src/components/ScrollBar.tsx
+++ src/components/ScrollBar.tsx
@@ -1,94 +1,94 @@
-import './ScrollBar.css'
-import { useState, useEffect} from 'react';
-import pyramid from '../assets/pyramid.png'
-import ResizeObserver from 'resize-observer-polyfill'
+import "./ScrollBar.css";
+import { useState, useEffect, useRef } from "react";
+import pyramid from "../assets/pyramid.png";
+import ResizeObserver from "resize-observer-polyfill";
 
-function ScrollBar( props: any ) {
-    // const [thumbTop, setThumbTop] = useState(0);
-    // const [scrollRef, setScrollRef] = useState<any>(null);
-    const scrollRef = props.content_ref;
-    const [clientHeight, setClientHeight] = useState(0);
-    const [scrollHeight, setScrollHeight] = useState(0);
-    const [thumbHeight, setThumbHeight] = useState('70px');
+function ScrollBar(props: any) {
+  const scrollRef = props.content_ref;
+  const scrollContainer = useRef(null);
+  const [thumbHeight, setThumbHeight] = useState("70px");
+  const [thumbTop, setThumbTop] = useState(0);
 
-    // const handleScroll = () => {
-    //     console.log('scrollg');
-    //     // if (scrollRef.current) {
-    //     //     const scrollBar = scrollRef.current as HTMLElement
-    //     //     const scrollHeight = scrollBar.scrollHeight;
-    //     //     const clientHeight = scrollBar.clientHeight;
-    //     //     const scrollTop = scrollBar.scrollTop;
-    //     //     const thumbRatio = scrollTop / (scrollHeight - clientHeight);
-    //     //     setThumbTop(thumbRatio * (clientHeight - (scrollBar.querySelector('#scroll-bar-thumb') as HTMLElement).offsetHeight));
-    //     // }
-    // };
-
-    const handleScroll = () => {
-        // console.log((scrollRef.current as HTMLElement).scrollTop)
-        // console.log(scrollRef.current.clientHeight);
-        // console.log(scrollRef.current.scrollHeight);
-        
-    };
-
-    const handleResize = () => {
-
-        if (scrollRef.current.scrollHeight !== scrollHeight || 
-            scrollRef.current.clientHeight !== clientHeight) {
-            setClientHeight(scrollRef.current.clientHeight);
-            setScrollHeight(scrollRef.current.scrollHeight);
-            setThumbHeight((previous) => {
-                if (scrollRef.current.clientHeight !== 0 && scrollRef.current.scrollHeight !== 0) {
-                    console.log(Math.round(scrollRef.current.clientHeight / scrollRef.current.scrollHeight * 100) + '%')
-                    return Math.round(scrollRef.current.clientHeight / scrollRef.current.scrollHeight * 100) + '%';
-                } else {
-                    return previous;
-                }
-            })
-        }
+  const handleScroll = () => {
+    var percentage_scrolled = 0;
+    if (scrollRef.current) {
+      percentage_scrolled = scrollRef.current.scrollTop / (scrollRef.current.scrollHeight - scrollRef.current.clientHeight);
     }
+    setThumbTop((previous) => {
+      if (scrollContainer.current) {
+        const outer_height = (scrollContainer.current as HTMLElement).clientHeight
+        const thumb_height = (scrollContainer.current as HTMLElement).children[0].clientHeight;
+        return outer_height-thumb_height < 3 ? 0 : (outer_height-thumb_height-3) * percentage_scrolled;
+      }
+      return previous; 
+    });
+    
+  };
 
-    useEffect(() => {
-        // setScrollRef(props.content_ref);
-        if (scrollRef) {
-            const resizeObserver = new ResizeObserver(() => {
-                handleResize();
-            });
-            
-            for (const child of scrollRef.current.children){
-                resizeObserver.observe(child);
-            }
+  const handleResize = () => {
+    setThumbHeight((previous) => {
+      if (scrollRef.current.clientHeight !== 0 && scrollRef.current.scrollHeight !== 0) {
+        // console.log(Math.round(scrollRef.current.clientHeight / scrollRef.current.scrollHeight * 100) + '%')
+        return (
+          Math.round((scrollRef.current.clientHeight / scrollRef.current.scrollHeight) * 100) + "%"
+        );
+      }
+      return previous;
+    });
+  };
 
-            scrollRef.current.addEventListener('scroll', handleScroll);
-            return () => {
-                scrollRef.current.removeEventListener('scroll', handleScroll);
-                resizeObserver.disconnect();
-            }
-        }
-    }, [scrollRef]); 
-    // This creates a dependecy on the props.content_ref.current being loaded in order to load this hook
+  useEffect(() => {
+    // setScrollRef(props.content_ref);
+    handleResize();
+    if (scrollRef) {
+      const resizeObserver = new ResizeObserver(() => {
+        handleResize();
+      });
 
+      for (const child of scrollRef.current.children) {
+        resizeObserver.observe(child);
+      }
 
-    return (
-        <div id='scroll-bar-margin'>
-            <div id='scroll-bar-container'>
-                <div className='outer-scroll-button'>
-                    <div className='inner-scroll-button' id='scroll-up'>
-                        <img className="pyramid" src={pyramid}></img>
-                    </div>
-                </div>
-                <div id='tread'>
-                    <div style={{height: thumbHeight}} className='outer-scroll-button' id='scroll-bar-thumb-outer'>
-                        <div  className='inner-scroll-button' id='scroll-bar-thumb-inner'></div>
-                    </div>
-                </div>
-                <div className='outer-scroll-button'>
-                    <div className='inner-scroll-button' id = 'scroll-down'>
-                        <img id='upside-down-pyramid' className="pyramid" src={pyramid}></img>
-                    </div>
-                </div>
-            </div>
-        </div> 
-    )
+      scrollRef.current.addEventListener("scroll", handleScroll);
+      return () => {
+        scrollRef.current.removeEventListener("scroll", handleScroll);
+        resizeObserver.disconnect();
+      };
+    }
+  }, [scrollRef]);
+  // This creates a dependecy on the props.content_ref.current being loaded in order to load this hook
+
+  return (
+    <div id="scroll-bar-margin">
+      <div id="scroll-bar-container">
+        <div className="outer-scroll-button">
+          <div className="inner-scroll-button" id="scroll-up">
+            <img className="pyramid" src={pyramid}></img>
+          </div>
+        </div>
+        <div ref={scrollContainer} id="tread">
+          <div
+            style={{ height: thumbHeight, top: thumbTop }}
+            className="outer-scroll-button"
+            id="scroll-bar-thumb-outer"
+          >
+            <div
+              className="inner-scroll-button"
+              id="scroll-bar-thumb-inner"></div>
+          </div>
+        </div>
+        <div className="outer-scroll-button">
+          <div className="inner-scroll-button" id="scroll-down">
+            <img
+              id="upside-down-pyramid"
+              className="pyramid"
+              src={pyramid}
+            ></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ScrollBar
+export default ScrollBar;
--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,39 +1,38 @@
-import './TextEditor.css'
-import ScrollBar from './ScrollBar'
-import { useRef } from 'react'
+import "./TextEditor.css";
+import ScrollBar from "./ScrollBar";
+import { useRef } from "react";
 
 function TextEditor() {
-    const scrollableContentRef = useRef(null);
+  const scrollableContentRef = useRef(null);
 
-    // function handleScroll() {
-    //     console.log('scroll');
-    // }
-    return (
-        <div id='text-editor-container'>
-            <div id='text-body-container'>
-                <div id='text-editor-inner' ref={scrollableContentRef}>
-                    <div id='text-container'>
-                        <h1 className='project-title'>Batle.us</h1>
-                        <p className='project-body'>
-                            {`Currently developing a multiplayer game usingg. Angular, 
+  // function handleScroll() {
+  //     console.log('scroll');
+  // }
+  return (
+    <div id="text-editor-container">
+      <div id="text-body-container">
+        <div id="text-editor-inner" ref={scrollableContentRef}>
+          <div id="text-container">
+            <h1 className="project-title">Batle.us</h1>
+            <p style={{ marginBottom: 250 }} className="project-body">
+              {`Currently developing a multiplayer game usingg. Angular, 
                             Tailwind, Node.js, Express.js, and AWS along with libraries 
-                            such as D3.js, WebGL, and socket.io. Utilized Git for source control control conntorl contorl peanut peanuit peanut
-                            control. (Link: `}<a href='http://www.batle.us/' target='_blank'>www.batle.us</a>)
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            Here
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            Here
-                            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-                            EA SPORTS<br/>
-                            in game<br/>
-                            yenno<br/>
-                        </p>
-                    </div>
-                </div>
-                <ScrollBar content_ref={scrollableContentRef} />
-            </div>
+                            such as D3.js, WebGL, and socket.io. Utilized Git for source 
+                            control control conntorl contorl peanut peanuit peanut
+                            control. (Link: `}
+              <a href="http://www.batle.us/" target="_blank">
+                www.batle.us
+              </a>
+              ) Here Here EA SPORTS in game yenno
+            </p>
+            <p style={{ marginBottom: 420 }}>t</p>
+            <p>margin</p>
+          </div>
         </div>
-    )
+        <ScrollBar content_ref={scrollableContentRef} />
+      </div>
+    </div>
+  );
 }
 
-export default TextEditor
+export default TextEditor;
