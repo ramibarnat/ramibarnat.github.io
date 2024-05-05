ee9e78530c5e7a0d83b3c4188bc2520a750d2e88
Ram Sauce <ramisouguir@gmail.com>
2024-05-05 04:14:11 -0400

    added content to projects window

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -1,5 +1,6 @@
 import "./TextEditor.css";
 import ScrollBar from "./ScrollBar";
+import batle from "../assets/batle_screen.png";
 import { useRef } from "react";
 
 function TextEditor() {
@@ -14,19 +15,21 @@ function TextEditor() {
         <div id="text-editor-inner" ref={scrollableContentRef}>
           <div id="text-container">
             <h1 className="project-title">Batle.us</h1>
-            <p style={{ marginBottom: 250 }} className="project-body">
-              {`Currently developing a multiplayer game usingg. Angular, 
+            <p style={{ marginBottom: 20 }} className="project-body">
+              I am currently developing a multiplayer game usingg. Angular, 
                             Tailwind, Node.js, Express.js, and AWS along with libraries 
-                            such as D3.js, WebGL, and socket.io. Utilized Git for source 
-                            control control conntorl contorl peanut peanuit peanut
-                            control. (Link: `}
+                            such as D3.js, three.js, and socket.io. Utilized Git for source 
+                            control. (Link: {` `}
               <a href="http://www.batle.us/" target="_blank">
                 www.batle.us
-              </a>
-              ) Here Here EA SPORTS in game yenno
+              </a>) 
             </p>
-            <p style={{ marginBottom: 420 }}>t</p>
-            <p>margin</p>
+            <img className="text-editor-image" src={batle}></img>
+            <p className="project-body">
+              The website current has one minigame which utilizes 3D rendering
+              libraries and geojson data to allow users to guess the country on
+              the globe.
+            </p>
           </div>
         </div>
         <ScrollBar content_ref={scrollableContentRef} />
