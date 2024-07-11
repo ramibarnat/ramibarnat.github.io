49ea271c5a0a5380d2430a4935bd779b05f8c9ab
Ram Sauce <ramisouguir@gmail.com>
2024-07-10 22:32:22 -0400

    changed linkedin url

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,4 @@
 export const links = {
     github_url: 'https://github.com/RamiBarnat',
-    linkedin_url: "https://www.linkedin.com/in/rami-souguir-9732761aa/",
+    linkedin_url: "https://www.linkedin.com/in/rami-barnat-9732761aa/",
 }
