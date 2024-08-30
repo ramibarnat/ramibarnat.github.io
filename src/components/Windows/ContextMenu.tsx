89e3479abf5c5a564693b3a4c7b4487443dc34e3
Ram Sauce <ramisouguir@gmail.com>
2024-08-30 04:12:10 -0400

    working on context menu still

--- src/components/Windows/ContextMenu.tsx
+++ src/components/Windows/ContextMenu.tsx
@@ -6,13 +6,19 @@ function ContextMenu({x_pos, y_pos}: {x_pos: number, y_pos: number}) {
     return (
         <div style={{top: y_pos, left: x_pos}} className='context-menu-container'>
             <div className='context-menu-inner'>
-                <div className='context-menu-item'>
-                    New
-                    <img className="new-item-pyramid-image" src={pyramid}/>
-                    <div className='new-item-selector'>
 
+                {/* Create new item */}
+                <div className='context-menu-item create-new-item'>
+                    New
+                    <img className='new-item-pyramid-image' src={pyramid}/>
+                    <div className='new-item-selector'>
+                        <div className='context-menu-inner'>
+                            <span className='context-menu-item'>Folder</span>
+                            <span className='context-menu-item'>Text File</span>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     )
