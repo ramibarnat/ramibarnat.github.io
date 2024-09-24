5230e74ed1959ee5dfc8dbfac883068608779326
Rami Barnat <ramisouguir@gmail.com>
2024-09-24 15:33:03 -0400

    fixed mobile problem with grain image

--- src/components/Apps/Folder.tsx
+++ src/components/Apps/Folder.tsx
@@ -1,7 +1,10 @@
+import { useContext } from "react"
 import WindowInsideBorder from "../Windows/WindowInsideBorder"
+import { FileSystemContext } from "../File System/FileSystemContext"
 
 function Folder({id}: {id: string}) {
-
+    const { folders } = useContext(FileSystemContext)
+    
     return (
         <WindowInsideBorder id={id}>
 
